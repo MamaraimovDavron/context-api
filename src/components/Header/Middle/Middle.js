abe8a3c8f29c04cf83b647596b9ccbe4f05ed717
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-10-25 16:09:44 +0500

    header done

---
+++ src/components/Header/Middle/Middle.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./middle.css";
+
+export class Middle extends Component {
+  render() {
+    return <div className="container">Middle</div>;
+  }
+}
+
+export default Middle;
